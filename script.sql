0540dc2f2965586c61ae003f329c4fa9890a2bb5
Mike Winchester <aleminapavon30@gmail.com>
2024-09-24 13:43:12 -0600

    updated database script

--- script.sql
+++ script.sql
@@ -1,55 +1,65 @@
-CREATE DATABASE GESTIONPROYECTOS;
+CREATE DATABASE gestion_proyectos;
 
-USE GESTIONPROYECTOS;
+USE gestion_proyectos;
+ 
 
 CREATE TABLE rol(
-    idrol int AUTO_INCREMENT PRIMARY KEY ,
+    id_rol int AUTO_INCREMENT PRIMARY KEY ,
     descripcion varchar(50)
 );
 
 CREATE TABLE usuario(
-    idusuario int AUTO_INCREMENT PRIMARY KEY ,
+    id_usuario int AUTO_INCREMENT PRIMARY KEY ,
     nombre varchar(50),
     apellido varchar(50),
     correo varchar(100),
-    contrasena varchar (100)
+    contrasenia varchar (100)
 );
 
 CREATE TABLE estado(
-    idestado int AUTO_INCREMENT PRIMARY KEY ,
+    id_estado int AUTO_INCREMENT PRIMARY KEY ,
     descripcion varchar(50)
 );
 
 CREATE TABLE proyecto(
-    idproyecto int AUTO_INCREMENT PRIMARY KEY ,
+    id_proyecto int AUTO_INCREMENT PRIMARY KEY ,
     nombre varchar(50),
     descripcion varchar(200),
     fechainicio date,
     fechafinalizacion date,
-    idestado int,
+    id_estado int,
     administrador int,
-    foreign key(idestado) references estado(idestado),
-    foreign key(administrador) references usuario(idusuario)
+    foreign key(id_estado) references estado(id_estado),
+    foreign key(administrador) references usuario(id_usuario)
 );
 
 CREATE TABLE tarea(
-    idtarea int AUTO_INCREMENT PRIMARY KEY,
+    id_tarea int AUTO_INCREMENT PRIMARY KEY,
     titulo varchar (100),
     descripcion varchar(200),
     responsable int,
-    idproyecto int,
-    idestado int,
-    foreign key(responsable) references usuario(idusuario),
-    foreign key(idproyecto) references proyecto(idproyecto),
-    foreign key(idestado) references estado(idestado)
+    id_proyecto int,
+    id_estado int,
+    foreign key(responsable) references usuario(id_usuario),
+    foreign key(id_proyecto) references proyecto(id_proyecto),
+    foreign key(id_estado) references estado(id_estado)
 );
 
 CREATE TABLE usuarioxproyectoxrol(
-    idusuario int,
-    idproyecto int,
-    idrolUsuario int,
-    PRIMARY KEY(idusuario, idproyecto, idrolUsuario),
-    foreign key (idusuario) references usuario(idusuario),
-    foreign key (idproyecto) references proyecto(idproyecto),
-    foreign key (idrolUsuario) references rol(idrol)
+    id_usuario int,
+    id_proyecto int,
+    id_rol int,
+    PRIMARY KEY(id_usuario, id_proyecto, id_rol),
+    foreign key (id_usuario) references usuario(id_usuario),
+    foreign key (id_proyecto) references proyecto(id_proyecto),
+    foreign key (id_rol) references rol(id_rol)
+);
+
+
+CREATE TABLE usuarioxtarea(
+    id_usuario int,
+    id_tarea int,
+    PRIMARY KEY(id_usuario, id_tarea),
+    foreign key (id_usuario) references usuario(id_usuario),
+    foreign key (id_tarea) references tarea(id_tarea)
 );
