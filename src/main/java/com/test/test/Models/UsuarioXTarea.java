fb6606e8edf24b1b816da0d3d4111be41fe9e2df
Mike Winchester <aleminapavon30@gmail.com>
2024-09-24 22:48:47 -0600

    finished tarea and usuarioxtarea models

--- src/main/java/com/test/test/Models/UsuarioXTarea.java
+++ src/main/java/com/test/test/Models/UsuarioXTarea.java
@@ -1,7 +1,13 @@
 package com.test.test.Models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,8 +22,16 @@ import lombok.Setter;
 @Setter
 public class UsuarioXTarea {
     
-    
-    private int idUsuario;
+    @Id
+    @Column(name = "id_usuario_tarea")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUsuarioXTarea;
 
-    private int idTarea;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_tarea")
+    private Tarea tarea;
 }
