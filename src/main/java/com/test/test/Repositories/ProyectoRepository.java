774404ec1403290a413119d1804e31c5a3fce0b4
Ricardo Guardiola <ricardoguardiolahn@gmail.com>
2024-09-24 23:37:19 -0600

    test

--- src/main/java/com/test/test/Repositories/ProyectoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.test.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.test.Models.Proyecto;
-
-public interface ProyectoRepository extends JpaRepository<Proyecto, Integer>{
-    
-}
