1ca7982b14841585b618d8ca8b79d3cea3b53721
Ricardo Guardiola <ricardoguardiolahn@gmail.com>
2024-09-24 22:55:51 -0600

    proyecto and usuario_proyecto_rol done

--- src/main/java/com/test/test/Models/Proyecto.java
+++ src/main/java/com/test/test/Models/Proyecto.java
@@ -1,6 +1,14 @@
 package com.test.test.Models;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +23,32 @@ import lombok.Setter;
 @Setter
 public class Proyecto {
     
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proyecto")
     private int idProyecto;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "fechainicio")
+    private Date fechainicio;
+
+    @Column(name = "fechafinalizacion")
+    private Date fechafinalizacion;
+
+    @ManyToOne
+    @JoinColumn(name = "idEstado", referencedColumnName = "idEstado")
+    private Estado estado;
+
+    @ManyToOne()
+    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario")
+    private Usuario Administrador;
+
 
     
 }
--- src/main/java/com/test/test/Models/UsuarioXProyectoXRol.java
+++ src/main/java/com/test/test/Models/UsuarioXProyectoXRol.java
@@ -1,5 +1,38 @@
 package com.test.test.Models;
 
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "usuarioxproectoxrol")
 public class UsuarioXProyectoXRol {
-    
+
+    @Id
+    @Column(name = "id_usuario_proyecto_rol")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUsuarioProyectoRol;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_proyecto", referencedColumnName = "id_proyecto")
+    private Proyecto proyecto;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_rol", referencedColumnName = "id_rol")
+    private Rol rol;
 }
